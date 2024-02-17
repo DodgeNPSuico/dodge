d3882996dfb80ca31d77f6fe51505366a8665bd8
DodgeNPSuico <dodgesuico@ckcm.edu.ph>
2024-02-17 13:38:58 +0800

    comit again

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const PORT = process.env.PORT || 1234;
 const app = express();
 
 app.use(cors());
-app.use("/api/expenses", expenseRouter);
 
 app.use(express.json());
+
+app.use("/api/expenses", expenseRouter);
 
 app.listen(PORT, ()=> console.log(`server running from port: ${PORT}`));
